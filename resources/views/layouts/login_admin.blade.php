ac5ade5807105c4ae9e5371fcf5b103cd8977a46
Akbar Anung Yudha Saputra <akbarsaputra45@hotmail.com>
2019-01-21 19:00:09 +0700

    assets for admin and home

--- resources/views/layouts/login_admin.blade.php
+++ resources/views/layouts/login_admin.blade.php
@@ -8,16 +8,16 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Login | Tickle</title>
   <!-- plugins:css -->
-	<link rel="stylesheet" href="{{ asset('vendors/iconfonts/mdi/css/materialdesignicons.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('vendors/css/vendor.bundle.base.css') }}">
-  <link rel="stylesheet" href="{{ asset('vendors/css/vendor.bundle.addons.css') }}">
+	<link rel="stylesheet" href="{{ asset('admin/vendors/iconfonts/mdi/css/materialdesignicons.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('admin/vendors/css/vendor.bundle.base.css') }}">
+  <link rel="stylesheet" href="{{ asset('admin/vendors/css/vendor.bundle.addons.css') }}">
   <!-- endinject -->
   <!-- plugin css for this page -->
   <!-- End plugin css for this page -->
   <!-- inject:css -->
-  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  <link rel="stylesheet" href="{{ asset('admin/css/style.css') }}">
   <!-- endinject -->
-  <link rel="shortcut icon" href="{{ asset('images/favicon.png') }}" />
+  <link rel="shortcut icon" href="{{ asset('admin/images/favicon.png') }}" />
 </head>
 
 <body>
@@ -61,14 +61,14 @@
   </div>
   <!-- container-scroller -->
 	<!-- plugins:js -->
-  <script src="{{ asset('vendors/js/vendor.bundle.base.js') }}"></script>
-  <script src="{{ asset('vendors/js/vendor.bundle.addons.js') }}"></script>
+  <script src="{{ asset('admin/vendors/js/vendor.bundle.base.js') }}"></script>
+  <script src="{{ asset('admin/vendors/js/vendor.bundle.addons.js') }}"></script>
   <!-- endinject -->
   <!-- Plugin js for this page-->
   <!-- End plugin js for this page-->
   <!-- inject:js -->
-  <script src="{{ asset('js/off-canvas.js') }}"></script>
-  <script src="{{ asset('js/misc.js') }}"></script>
+  <script src="{{ asset('admin/js/off-canvas.js') }}"></script>
+  <script src="{{ asset('admin/js/misc.js') }}"></script>
   <!-- endinject -->
   <!-- Custom js for this page-->
   <!-- End custom js for this page-->
--- resources/views/master_petugas.blade.php
+++ resources/views/master_petugas.blade.php
@@ -7,19 +7,19 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>@yield('title') | Tickle</title>
   <!-- plugins:css -->
-  <link rel="stylesheet" href="{{ asset('vendors/iconfonts/mdi/css/materialdesignicons.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('vendors/css/vendor.bundle.base.css') }}">
-  <link rel="stylesheet" href="{{ asset('vendors/css/vendor.bundle.addons.css') }}">
+  <link rel="stylesheet" href="{{ asset('admin/vendors/iconfonts/mdi/css/materialdesignicons.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('admin/vendors/css/vendor.bundle.base.css') }}">
+  <link rel="stylesheet" href="{{ asset('admin/vendors/css/vendor.bundle.addons.css') }}">
   <!-- endinject -->
   <!-- plugin css for this page -->
-  <link rel="stylesheet" href="{{ asset('vendors/iconfonts/font-awesome/css/font-awesome.css') }}">
+  <link rel="stylesheet" href="{{ asset('admin/vendors/iconfonts/font-awesome/css/font-awesome.css') }}">
   <!-- End plugin css for this page -->
   <!-- inject:css -->
-  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  <link rel="stylesheet" href="{{ asset('admin/css/style.css') }}">
   <!-- endinject -->
-  <link rel="stylesheet" href="{{ asset('vendors/datatables/datatables.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('admin/vendors/datatables/datatables.min.css') }}">
   @yield('style')
-  <link rel="shortcut icon" href="{{ asset('images/favicon.png') }}" />
+  <link rel="shortcut icon" href="{{ asset('admin/images/favicon.png') }}" />
 </head>
 
 <body>
@@ -28,10 +28,10 @@
     <nav class="navbar default-layout navbar-grad-1 col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
 			<div class="text-center navbar-brand-wrapper d-flex align-items-top justify-content-center">
         <a class="navbar-brand brand-logo" href="../../index.html">
-          <img src="{{ asset('images/logo.svg') }}" alt="logo" />
+          <img src="{{ asset('admin/images/logo.svg') }}" alt="logo" />
         </a>
         <a class="navbar-brand brand-logo-mini" href="../../index.html">
-          <img src="{{ asset('images/logo-mini.svg') }}" alt="logo" />
+          <img src="{{ asset('admin/images/logo-mini.svg') }}" alt="logo" />
         </a>
       </div>
       <div class="navbar-menu-wrapper d-flex align-items-center">
@@ -46,7 +46,7 @@
           <li class="nav-item dropdown d-none d-xl-inline-block">
             <a class="nav-link dropdown-toggle" id="UserDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
               <span class="profile-text">Hello, Richard V.Welsh !</span>
-              <img class="img-xs rounded-circle" src="{{ asset('images/faces/face1.jpg') }}" alt="Profile image">
+              <img class="img-xs rounded-circle" src="{{ asset('admin/images/faces/face1.jpg') }}" alt="Profile image">
             </a>
             <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="UserDropdown">
               <a class="dropdown-item p-0">
@@ -92,7 +92,7 @@
             <div class="nav-link">
               <div class="user-wrapper">
                 <div class="profile-image">
-                  <img src="{{ asset('images/faces/face1.jpg') }}" alt="profile image">
+                  <img src="{{ asset('admin/images/faces/face1.jpg') }}" alt="profile image">
                 </div>
                 <div class="text-wrapper">
                   <p class="profile-name">Richard V.Welsh</p>
@@ -162,20 +162,20 @@
   </div>
   <!-- container-scroller -->
   <!-- plugins:js -->
-  <script src="{{ asset('vendors/js/vendor.bundle.base.js') }}"></script>
-  <script src="{{ asset('vendors/js/vendor.bundle.addons.js') }}"></script>
+  <script src="{{ asset('admin/vendors/js/vendor.bundle.base.js') }}"></script>
+  <script src="{{ asset('admin/vendors/js/vendor.bundle.addons.js') }}"></script>
   <!-- endinject -->
   <!-- Plugin js for this page-->
   <!-- End plugin js for this page-->
   <!-- inject:js -->
-  <script src="{{ asset('js/off-canvas.js') }}"></script>
-  <script src="{{ asset('js/misc.js') }}"></script>
+  <script src="{{ asset('admin/js/off-canvas.js') }}"></script>
+  <script src="{{ asset('admin/js/misc.js') }}"></script>
   <!-- endinject -->
   <!-- Custom js for this page-->
   <!-- End custom js for this page-->
-  <script src="{{ asset('vendors/datatables/datatables.min.js') }}" charset="utf-8"></script>
-  <script src="{{ asset('vendors/datatables/buttons.bootstrap4.min.js') }}" charset="utf-8"></script>
-  <script src="{{ asset('vendors/datatables/vfs_fonts.js') }}" charset="utf-8"></script>
+  <script src="{{ asset('admin/vendors/datatables/datatables.min.js') }}" charset="utf-8"></script>
+  <script src="{{ asset('admin/vendors/datatables/buttons.bootstrap4.min.js') }}" charset="utf-8"></script>
+  <script src="{{ asset('admin/vendors/datatables/vfs_fonts.js') }}" charset="utf-8"></script>
   <script type="text/javascript">
     $(document).ready(function() {
       $('table').DataTable();
